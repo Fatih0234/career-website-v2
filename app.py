9e59080ce4134f43e0c3a6466dbab59752f4b0a8
fatihkarahan <sekanti02@gmail.com>
2023-03-04 13:51:28 +0300

    created dynamic jobpage

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -13,7 +13,11 @@ def index():
 def list_jobs():
     return jsonify(load_jobs_from_db())
 
+@app.route("/job/<job_id>")
+def get_job(job_id):
+    job = load_job_from_db(job_id)
 
+    return render_template("jobpage.html", job=job)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- database.py
+++ database.py
@@ -3,15 +3,27 @@ from sqlalchemy import create_engine, text
 import os
 
 
-database_uri = os.environ.get("DB_CONNECTION_STRING")
+database_uri = os.getenv("DB_CONNECTION_STRING")
 engine = create_engine(database_uri, connect_args=
                        {
                            'ssl': {'ssl_ca': "/etc/ssl/cert.pem"}
                         })
 
 
-
 def load_jobs_from_db():
     with engine.connect() as con:
         rs = con.execute(text("SELECT * FROM jobs"))
-        return rs.fetchall()
+        jobs = []
+        
+        for row in rs.all():
+            jobs.append(row._asdict())
+
+    return jobs
+
+
+def load_job_from_db(job_id):
+    with engine.connect() as con:
+        rs = con.execute(text("SELECT * FROM jobs WHERE id=:id"), {"id": job_id})
+        job = rs.first()._asdict()
+    return job
+    
